765b3c6086ddd3986b43304faa66868a42fba1fb
Thomas Keel <sndbytgkv1@verizon.net>
2016-09-12 01:36:37 -0400

    setup Jumbotron and background color

--- server.js
+++ server.js
@@ -5,6 +5,7 @@
 // dependencies
 var express = require('express');
 var app = express();
+var exphbs  = require('express-handlebars');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
